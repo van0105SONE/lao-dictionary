11d1a152da822a75af5cd0cce9267eddf2290dbc
van0105SONE <chanthakhaninsyxiengmai@gmail.com>
2025-08-03 23:00:46 +0700

    fixed: env

--- src/db/drizzle.ts
+++ src/db/drizzle.ts
@@ -1,6 +1,6 @@
-import { config } from "dotenv";
+
 import { drizzle } from 'drizzle-orm/neon-http';
 
-config({ path: ".env" }); // or .env.local
 
+console.log('env:     ', process.env.DATABASE_URL)
 export const db = drizzle(process.env.DATABASE_URL!);
