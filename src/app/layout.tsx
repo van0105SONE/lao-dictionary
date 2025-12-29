ed33d3ba190c615ae0b0740898871f96e95ef36e
van0105SONE <chanthakhaninsyxiengmai@gmail.com>
2025-12-29 12:28:25 +0700

    feat: google analytic

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
+import Script from "next/script";
 
 const myFont = localFont({
   src: "../../public/NotoSansLao_Condensed-Black.ttf",
@@ -72,6 +73,24 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        {/* Google Analytics */}
+        <Script
+          async
+          src="https://www.googletagmanager.com/gtag/js?id=G-L0191P70P6"
+          strategy="afterInteractive"
+        />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-L0191P70P6', {
+              page_path: window.location.pathname,
+            });
+          `}
+        </Script>
+      </head>
       <body className={myFont.className}>{children}</body>
     </html>
   );
